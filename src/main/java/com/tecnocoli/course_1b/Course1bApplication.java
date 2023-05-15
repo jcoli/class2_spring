d0c97011ae52dd5071c9ce93e17d5408aedf3390
Jeferson Coli <jefersoncoli@gmail.com>
2023-05-15 15:11:26 -0300

    class1 spring

---
+++ src/main/java/com/tecnocoli/course_1b/Course1bApplication.java
@@ -0,0 +1,13 @@
+package com.tecnocoli.course_1b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1bApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1bApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecnocoli/course_1b/Course1bApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecnocoli.course_1b;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course1bApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
