7b7dda616e1a8708b263390afb8fbf517fbfae04
Jeferson Coli <jefersoncoli@gmail.com>
2023-05-16 18:16:59 -0300

    Aula299

--- src/main/java/com/tecnocoli/course_1b/Course1bApplication.java
+++ src/main/java/com/tecnocoli/course_1b/Course1bApplication.java
@@ -2,6 +2,7 @@ package com.tecnocoli.course_1b;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class Course1bApplication {
