f784a9f8c073e61e6d941cd422b4d7602259b1b6
Jeferson Coli <jefersoncoli@gmail.com>
2023-05-17 16:43:28 -0300

    Aula299

--- src/main/java/com/tecnocoli/course_1b/entities/User.java
+++ src/main/java/com/tecnocoli/course_1b/entities/User.java
@@ -1,11 +1,20 @@
 package com.tecnocoli.course_1b.entities;
 
+
+
+import jakarta.persistence.*;
+
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
