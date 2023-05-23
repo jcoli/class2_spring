c1e732bcafa2ebf9e40d1d8e140407a75f7c461c
Jeferson Coli <jefersoncoli@gmail.com>
2023-05-23 16:26:54 -0300

    Aula303

--- src/main/java/com/tecnocoli/course_1b/entities/User.java
+++ src/main/java/com/tecnocoli/course_1b/entities/User.java
@@ -1,11 +1,10 @@
 package com.tecnocoli.course_1b.entities;
 
-
-
 import jakarta.persistence.*;
 
-
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
     }
@@ -73,6 +75,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -84,6 +90,7 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hash(getId());
     }
+
 
 
 }
